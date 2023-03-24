0cc04aeb5ccad8fc4cd06e127833b247d1b87990
Oliver <oliver_shore@outlook.com>
2023-03-24 16:23:56 +0800

    第一次提交

---
+++ hiot_cloud/app/src/main/java/com/example/hiot_cloud/test/mvptest/TestMVPActivity.java
@@ -0,0 +1,14 @@
+package com.example.hiot_cloud.test.mvptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestMVPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_mvpactivity);
+    }
+}
---
+++ hiot_cloud/app/src/main/java/com/example/hiot_cloud/test/mvptest/TestPresenter.java
@@ -0,0 +1,2 @@
+package com.example.hiot_cloud.test.mvptest;public class TestPresenter {
+}

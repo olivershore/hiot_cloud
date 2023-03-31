25bc8cd43ce056e09189d6f840e38e92c33ec1f5
Oliver <oliver_shore@outlook.com>
2023-03-31 10:08:39 +0800

    MVPtest,设置基类模板

--- hiot_cloud/app/src/main/java/com/example/hiot_cloud/test/mvptest/TestMVPActivity.java
+++ hiot_cloud/app/src/main/java/com/example/hiot_cloud/test/mvptest/TestMVPActivity.java
@@ -1,14 +1,67 @@
 package com.example.hiot_cloud.test.mvptest;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class TestMVPActivity extends AppCompatActivity {
+import com.example.hiot_cloud.R;
+import com.example.hiot_cloud.base.BaseActivity;
+import com.example.hiot_cloud.test.mvptest.model.User;
+
+public class TestMVPActivity extends BaseActivity<TestView, TestPresenter> implements TestView {
+    private TestPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        presenter = new TestPresenter();
         super.onCreate(savedInstanceState);
+        //
         setContentView(R.layout.activity_test_mvpactivity);
+        EditText etUsername = findViewById(R.id.et_user_name);
+        EditText etPassword = findViewById(R.id.et_password);
+        //
+        Button btnLogin = findViewById(R.id.btn_login);
+        //
+        final User user = new User();
+        // final TestPresenter presenter =new TestPresenter();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mvc,在这身份校验
+                user.setUserName(etUsername.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                presenter.login(user);
+//                login(user);
+
+
+            }
+        });
     }
+
+    @Override
+    public TestPresenter creatPresenter() {
+        return presenter;
+    }
+
+
+    @Override
+    public void showMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+//
+//    private void login(User user) {
+//        if ("lisi".equals(user.getUserName())&&"123".equals(user.getPassword())){
+//            Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+//        }else {
+//            Toast.makeText(this, "登录失败", Toast.LENGTH_SHORT).show();
+//        }
+//
+//
+//    }
 }
